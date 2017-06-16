5cae2c7e79998959380ab079c997e4df4973b492
Markus Hederström <d0namiz@gmail.com>
2017-06-16 20:06:47 +0200

    changed API url and added a limit

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -3,12 +3,13 @@ import { Http } from '@angular/http';
 
 @Injectable()
 export class SearchService {
+  private limit: number = 10;
 
   constructor(private http: Http) { }
 
   search (term: string) {
     return this.http
-      .get('http://localhost:9000/api/product/search?query=' + term)
+      .get(`http://localhost:9000/api/product?query=${term}&limit=${this.limit}`)
       .map((response) => response.json());
   }
 }
